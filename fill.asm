7c9c9edcfd48597602b61bcbffce5eef552ed964
Lars Kellogg-Stedman <lars@oddbit.com>
2023-07-06 19:19:56 -0400

    Fewer magic numbers

--- fill.asm
+++ fill.asm
@@ -1,30 +1,52 @@
 :BasicUpstart2(main)
 
-.pc = $c000
+.const numrows = 24
+.const numcols = 40
+.const char_x = 24
+
+.var screen_base = $0400
+.var screen_row_01 = screen_base
+.var screen_row_24 = $07c0
+
+* = $00fb "ZP" virtual
+target: .word 0
+
+* = $c000
 main:
   ldx #0
-  lda #24
+  lda #char_x
 fill_row_1:
-  sta $0400,X
+  sta screen_row_01,X
+  sta screen_row_24,X
   inx
-  cpx #40
+  cpx #numcols
   bne fill_row_1
 
-  lda #24
-  ldy #6
-  ldx #0
-  clc
+  lda #<screen_base
+  sta target
+  lda #>screen_base
+  sta target+1
+  lda #char_x
+  ldx #numrows
 fill_column_1:
-  sta $0400,X
-  sta $04f0,X
-  sta $05e0,X
-  sta $06d0,X
-  pha
-  txa
-  adc #40
-  tax
-  pla
-  dey
+  ldy #0
+  sta (target),y
+  ldy #(numcols-1)
+  sta (target),y
+  add(target, numcols)
+  dex
   bne fill_column_1
 
   rts
+
+.macro add(loc, val) {
+  clc
+  pha
+  lda loc
+  adc #<val
+  sta loc
+  lda loc+1
+  adc #>val
+  sta loc+1
+  pla
+}
