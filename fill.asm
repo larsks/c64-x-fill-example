77918dfd9932935a43e7afa2d8de653e47e01b31
Lars Kellogg-Stedman <lars@oddbit.com>
2023-07-06 10:41:43 -0400

    Rewrite without using example subroutine

--- fill.asm
+++ fill.asm
@@ -10,46 +10,21 @@ fill_row_1:
   cpx #40
   bne fill_row_1
 
-  ldy #0
-  ldx #24
-
-  //  Load screen base address ($0400) into location $fb
-  lda #$0
-  sta $fb
-  lda #$04
-  sta $fc
-
-  //  Load character "X" into A
   lda #24
+  ldy #6
+  ldx #0
+  clc
 fill_column_1:
-  //  Save A because add_words_imm changes it
+  sta $0400,X
+  sta $04f0,X
+  sta $05e0,X
+  sta $06d0,X
   pha
-
-  //  Increment target address by 40 characters
-  add_words_imm($fb, 40, $fb)
-
-  //  Restore A
+  txa
+  adc #40
+  tax
   pla
-
-  //  Write character to screen location
-  sta ($fb),y
-  dex
+  dey
   bne fill_column_1
 
   rts
-
-.macro add_words_imm(a, b, result) {
-    clc
-  .for(var byte = 0;  byte < 2; byte++) {
-    lda a + byte
-    adc #extract_byte(b, byte)
-    sta result + byte
-  }
-}
-
-.function extract_byte(value, byte_id) {
-  .var bits = byte_id * 8
-  .eval value = value >> bits
-  .return value & 255
-}
-
